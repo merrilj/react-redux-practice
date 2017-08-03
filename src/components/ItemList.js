e09fa5ffa4297249b6a16a0f7bbad2b6d34eac70
Merril Jeffs <merril@Merrils-MacBook-Pro.local>
2017-08-03 14:20:49 -0600

    initial commit/added delete feature

---
+++ src/components/ItemList.js
@@ -0,0 +1,58 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { itemsFetchData, itemsRemoveItem } from '../actions/items'
+
+import logo from '../logo.svg'
+import '../App.css'
+
+class ItemList extends Component {
+  componentDidMount() {
+    this.props.fetchData('http://5826ed963900d612000138bd.mockapi.io/items')
+  }
+
+  render() {
+    if (this.props.hasErrored) {
+      return <p>Sorry! There was an evil error loading the items!</p>
+    }
+
+    if (this.props.isLoading) {
+      return <p>Loading, please wait...</p>
+    }
+
+    return (
+      <ul>
+        {this.props.items.map((item, index) => (
+          <li className="font" key={item.id}>
+            {item.label}
+            <button onClick={() => this.props.removeItem(index)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+}
+
+ItemList.propTypes = {
+  fetchData: PropTypes.func.isRequired,
+  removeItem: PropTypes.func.isRequired,
+  items: PropTypes.array.isRequired,
+  hasErrored: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired
+}
+
+const mapStateToProps = (state) => {
+  return {
+    items: state.items,
+    hasErrored: state.itemsHasErrored,
+    isLoading: state.itemsIsLoading
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchData: (url) => dispatch(itemsFetchData(url)),
+    removeItem: (index) => dispatch(itemsRemoveItem(index))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ItemList)
---
+++ src/reducers/items.js
@@ -0,0 +1,35 @@
+export function itemsHasErrored(state = false, action) {
+    switch (action.type) {
+        case 'ITEMS_HAS_ERRORED':
+            return action.hasErrored
+
+        default:
+            return state
+    }
+}
+
+export function itemsIsLoading(state = false, action) {
+    switch (action.type) {
+        case 'ITEMS_IS_LOADING':
+            return action.isLoading
+
+        default:
+            return state
+    }
+}
+
+export function items(state = [], action) {
+    switch (action.type) {
+        case 'ITEMS_FETCH_DATA_SUCCESS':
+            return action.items
+
+        case 'ITEMS_REMOVE_ITEM':
+            return [
+                ...state.slice(0, action.index),
+                ...state.slice(action.index + 1)
+            ]
+
+        default:
+            return state
+    }
+}
